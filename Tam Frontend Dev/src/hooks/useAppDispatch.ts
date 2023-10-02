65a11da7a81c6d09b677512fa1dbce7736fb3347
Redis7 <redis.halili@sparklab.al>
2023-10-02 11:15:41 +0200

    Fixes

--- Tam Frontend Dev/src/hooks/useAppDispatch.ts
+++
@@ -1,9 +0,0 @@
-import { useDispatch } from 'react-redux';
-
-// store type
-import { AppDispatch } from 'redux/store';
-
-
-const useAppDispatch: () => AppDispatch = useDispatch;
-
-export default useAppDispatch;
--- Tam Frontend Dev/src/hooks/useAppSelector.ts
+++
@@ -1,9 +0,0 @@
-import { TypedUseSelectorHook, useSelector } from 'react-redux';
-
-// store type
-import { RootState } from 'redux/store';
-
-
-const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
-export default useAppSelector;
