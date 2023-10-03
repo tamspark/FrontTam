a0d200cd0cc0536f1c808bcb03a18e651cdb8ae2
Redis7 <redis.halili@sparklab.al>
2023-10-03 11:18:28 +0200

    Change password

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,11 +2,11 @@ import { FC } from "react";
 
 // style
 import {} from "./style/App.style";
-import Login from "Pages/Login/Login.component";
+import ResetPassword from "Pages/Authentication/ResetPassword/ResetPassword";
 
 const App: FC<{}> = () => {
   return <>
-  <Login/>
+  <ResetPassword/>
     </>;
 };
 
