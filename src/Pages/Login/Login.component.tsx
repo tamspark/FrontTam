8001624c6c1e451a4e0f7b918fb7ea40ba56d1f4
Redis7 <redis.halili@sparklab.al>
2023-10-06 16:41:46 +0200

    Password types

--- src/Pages/Login/Login.component.tsx
+++ src/Pages/Login/Login.component.tsx
@@ -5,13 +5,18 @@ import { loginUser, clearUser, logoutUser } from "redux/authSlicer";
 import { Input } from "App/style/App.style";
 import { Button } from "App/style/App.style";
 import { StyledForm } from "App/style/App.style";
+import { InputGroup } from "App/style/App.style";
+import { ToggleButton } from "App/style/App.style";
 import { AppDispatch } from "redux/store";
 import { useNavigate } from "react-router";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Login: FC<{}> = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
 
   const dispatch: AppDispatch = useDispatch();
 
@@ -27,6 +32,10 @@ const Login: FC<{}> = () => {
 
     console.log(typeof userCredentials);
     await dispatch(loginUser(userCredentials));
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword); // Toggle the state when the button is clicked
   };
 
   // const handleLogout = (userId: number) => {
@@ -64,26 +73,34 @@ const Login: FC<{}> = () => {
             setEmail(e.target.value)
           }
         ></Input>
-        <Input
-          placeholder="Password"
-          type="password"
-          fontFamily="sanf-serif"
-          fontSize="12px"
-          fontWeight="700"
-          borderbottomrightradius="20px"
-          bordertoprightradius="20px"
-          border="1px solid black"
-          width="100%"
-          height="40px"
-          backgroundcolor="#FFFFFF"
-          borderradius="10px"
-          paddingleft="5px"
-          padding="0 10px"
-          value={password}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setPassword(e.target.value)
-          }
-        ></Input>
+            
+            <InputGroup>
+          <Input
+            type={showPassword ? "text" : "password"}
+            value={password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setPassword(e.target.value)
+            }
+            placeholder="Password"
+            fontFamily="sanf-serif"
+            fontSize="12px"
+            fontWeight="700"
+            borderbottomrightradius="20px"
+            bordertoprightradius="20px"
+            border="1px solid black"
+            width="100%"
+            height="40px"
+            backgroundcolor="#FFFFFF"
+            borderradius="10px"
+            paddingleft="5px"
+            padding="0 10px"
+          />
+          <ToggleButton type="button" onClick={togglePasswordVisibility}>
+            <FontAwesomeIcon
+              icon={showPassword ? faEyeSlash : faEye}
+            />
+          </ToggleButton>
+        </InputGroup>
         <Button
           h="40px"
           w="100%"
@@ -95,7 +112,7 @@ const Login: FC<{}> = () => {
         >
           Submit
         </Button>
-      
+     
       </StyledForm>
     </>
   );
