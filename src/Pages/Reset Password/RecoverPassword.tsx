4e33d3bf3f3fefa6e550f60b3afafef0cd5f33d5
giselathellipi <109949845+giselathellipi@users.noreply.github.com>
2023-10-06 10:21:00 +0200

    fix api call

--- src/Pages/Reset Password/RecoverPassword.tsx
+++ src/Pages/Reset Password/RecoverPassword.tsx
@@ -7,81 +7,91 @@ import { StyledForm } from "App/style/App.style";
 import { AppDispatch } from "redux/store";
 import { useDispatch } from "react-redux";
 import { resetPassword } from "redux/Auth/recoverPasword/RecoverPasswordSlice";
+import { useNavigate } from "react-router";
 const ResetPassword: FC<{}> = () => {
-  const handleClick = () => {
-    console.log("U klikua!");
-  };
-  const [newPassword,setNewPassword]=useState<string>("")
-  const [confirmPassword,setConfirmPassword]=useState<string>("")
- 
+  const navigate = useNavigate();
+  const [newPassword, setNewPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [passwordsMatch, setPasswordsMatch] = useState<boolean>(true);
   const dispatch: AppDispatch = useDispatch();
 
   const handleResetPassClick = async (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
     e.preventDefault();
-
- 
-
-    console.log(typeof newPassword);
-    await dispatch(resetPassword(newPassword));
+    if (newPassword !== confirmPassword) {
+      setPasswordsMatch(false);
+      return;
+    }
+    setPasswordsMatch(true);
+    dispatch(resetPassword(newPassword))
+    .then((resultAction) => {
+      if (resultAction) {
+        // Password reset was successful, you can handle success here
+        console.log("Password reset successful");
+        navigate("/auth/login");
+      } else {
+        // Password reset failed, you can handle the error here
+        console.error("Password reset failed:");
+      }
+    });
   };
-
   return (
     <>
-    <StyledForm height="280px">
-      <h1>Reset Password!</h1>
-      <Input
-        placeholder="New Password"
-        type="Passowrd"
-        fontFamily="sanf-serif"
-        fontSize="12px"
-        fontWeight="700"
-        borderbottomrightradius="20px"
-        bordertoprightradius="20px"
-        border="1px solid black"
-        width="100%"
-        height="40px"
-        backgroundcolor="#FFFFFF"
-        borderradius="10px"
-        paddingleft="5px"
-        padding="0 10px"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setNewPassword(e.target.value)
-        }
-      ></Input>
-      <Input
-        placeholder="Confirm Password"
-        type="password"
-        fontFamily="sanf-serif"
-        fontSize="12px"
-        fontWeight="700"
-        borderbottomrightradius="20px"
-        bordertoprightradius="20px"
-        border="1px solid black"
-        width="100%"
-        height="40px"
-        backgroundcolor="#FFFFFF"
-        borderradius="10px"
-        paddingleft="5px"
-        padding="0 10px"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setConfirmPassword(e.target.value)
-        }
-      ></Input>
-      <Button
-        h="40px"
-        w="100%"
-        variant="primary"
-      
-        borderRadius="5px"
-        fontFamily="sanf-sarif"
-        fontSize="18px"
-        fontWeight="700"
-        onClick={handleResetPassClick}
-      >
-        Submit
-      </Button>
+      <StyledForm height="280px">
+        <h1>Reset Password!</h1>
+        <Input
+          placeholder="New Password"
+          type="password"
+          fontFamily="sanf-serif"
+          fontSize="12px"
+          fontWeight="700"
+          borderbottomrightradius="20px"
+          bordertoprightradius="20px"
+          border="1px solid black"
+          width="100%"
+          height="40px"
+          backgroundcolor="#FFFFFF"
+          borderradius="10px"
+          paddingleft="5px"
+          padding="0 10px"
+          value={newPassword}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setNewPassword(e.target.value)
+          }
+        ></Input>
+        <Input
+          placeholder="Confirm Password"
+          type="password"
+          fontFamily="sanf-serif"
+          fontSize="12px"
+          fontWeight="700"
+          borderbottomrightradius="20px"
+          bordertoprightradius="20px"
+          border="1px solid black"
+          width="100%"
+          height="40px"
+          backgroundcolor="#FFFFFF"
+          borderradius="10px"
+          paddingleft="5px"
+          padding="0 10px"
+          value={confirmPassword}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setConfirmPassword(e.target.value)
+          }
+        ></Input>
+        <Button
+          h="40px"
+          w="100%"
+          variant="primary"
+          borderRadius="5px"
+          fontFamily="sanf-sarif"
+          fontSize="18px"
+          fontWeight="700"
+          onClick={handleResetPassClick}
+        >
+          Submit
+        </Button>
       </StyledForm>
     </>
   );
--- src/redux/Auth/recoverPasword/RecoverPasswordSlice.tsx
+++ src/redux/Auth/recoverPasword/RecoverPasswordSlice.tsx
@@ -1,80 +1,86 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import axios from "axios";
-
-
+import { useNavigate } from "react-router";
 interface RecoverPass {
-    password: string;
-    acessToken: string | null;
+  password: string;
+  token: string | null;
 }
- interface AuthRecoverPass {
-    user: RecoverPass | null;
-    isAuthenticated: boolean;
-    error: string | null;
- }
+interface AuthRecoverPass {
+  user: RecoverPass | null;
+  isAuthenticated: boolean;
+  error: string | null;
+}
 const initialState: AuthRecoverPass = {
-    user: null,
-    isAuthenticated: false,
-    error: null,
-}
+  user: null,
+  isAuthenticated: false,
+  error: null,
+};
 
-const accessToken = sessionStorage.getItem("accessToken");
 export const resetPassword = createAsyncThunk(
-    'resetPassword/reset',
-    async (newPassword: string, { rejectWithValue }) => {
-        
-      try {
-     
-        const response = await axios.post('http://192.168.10.213:8080/TAM/savepassword/{accessToken}', {
-            password: newPassword,
-            accessToken,
-        });
-        const responseData = response.data;
+  "reset/resetPassword",
+  async (newPassword: string, { rejectWithValue }) => {
+    const navigate = useNavigate();
+    const currentUrl = window.location.href;
+    try {
+      const url = new URL(currentUrl);
 
-        console.log(responseData);
-  
-        localStorage.setItem("user", JSON.stringify(responseData));
-        
-        if (response.status !== 200) {
-            return rejectWithValue(responseData.error.message);
-          }
-    
-          localStorage.setItem("user", JSON.stringify(responseData));
-    
-          return responseData;
-        } catch (error) {
-          console.log("Error in loginUser:", error);
-    
-          return rejectWithValue("Login failed");
+      const pathnameParts = url.pathname.split("/");
+      const token = pathnameParts[pathnameParts.length - 1];
+      console.log("Token:", token);
+      console.log(``);
+      const response = await axios.post(
+        "http:/localhost/savepassword/{token}",
+        {
+          password: newPassword,
         }
+      );
+      const responseData = response.data.body;
+
+      console.log(responseData);
+
+      localStorage.setItem("user", JSON.stringify(responseData));
+
+      if (response.status !== 200) {
+        return rejectWithValue(responseData.error.message);
+        navigate("/auth/login");
       }
-    );
 
+      // localStorage.setItem("user", JSON.stringify(responseData));
 
-const resetPasswordSlice  = createSlice({
-    name: "resetPassword",
-    initialState,
-    reducers: {
-      setUser: (state, action: PayloadAction<RecoverPass>) => {
+      return responseData;
+    } catch (error) {
+      console.log("Error in loginUser:", error);
+
+      return rejectWithValue("Login failed");
+    }
+  }
+);
+
+const resetPasswordSlice = createSlice({
+  name: "recoverPassword",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<RecoverPass>) => {
+      state.user = action.payload;
+      state.isAuthenticated = true;
+    },
+    clearUser: (state) => {
+      state.user = null;
+      state.isAuthenticated = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(resetPassword.fulfilled, (state, action) => {
         state.user = action.payload;
         state.isAuthenticated = true;
-      },
-      clearUser: (state) => {
-        state.user = null;
+      })
+      .addCase(resetPassword.rejected, (state, action) => {
         state.isAuthenticated = false;
-      },
-    },
-    extraReducers: (builder) => {
-      builder
-        .addCase(resetPassword.fulfilled, (state, action) => {
-          state.user = action.payload;
-          state.isAuthenticated = true;
-        })
-        .addCase(resetPassword.rejected, (state, action) => {
-          state.isAuthenticated = false;
-          state.user = null;
+        state.user = null;
         //   state.error = action.payload as string | null;
-        });
-    },
-  });
-  export const { setUser, clearUser } = resetPasswordSlice.actions;
+      });
+  },
+});
+export const { setUser, clearUser } = resetPasswordSlice.actions;
 export default resetPasswordSlice.reducer;
