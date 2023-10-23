c2b1ce823c3bb89d4f898ecb0c8d4e2bc7c86099
Redis7 <redis.halili@sparklab.al>
2023-10-23 09:40:35 +0200

    Deny access to pages if user is not logged in

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,34 +6,42 @@ import ResetPassword from "Pages/Reset Password/ResetPassword";
 import Login from "Pages/Login/Login.component";
 import AuthPage from "Pages/Auth/Auth";
 import Verification from "Pages/SecondStepAuth/SecondStep.component";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
 import ForgetPassword from "../Pages/ChangePassword/ForgetPassword.component";
 import Register from "../Pages/Register/Register.component";
 import SavePasword from "../Pages/savePassword/SavePassword.component";
 import ApartmentPage from "Pages/ApartmentPage/ApartmentPage.component";
 import ApartmentCard from "Pages/ApartmentCard/ApartmentCard.component";
+import { useSelector } from "react-redux";
+import { RootState } from "redux/store";
 
 const App: FC<{}> = () => {
+  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/auth" element={<AuthPage />}>
-            <Route path="login" element={<Login />} index />
-            <Route
-              path="tam/resetPassword/:token"
-              element={<ResetPassword />}
-            />
-            <Route path="tam/registration/:token" element={<SavePasword />} />
-            <Route path="apartmentpage" element={<ApartmentPage />} />
-            <Route path="apartmentcard/:id" element={<ApartmentCard />} />
-            <Route path="forgetpassword" element={<ForgetPassword />} />
-            <Route path="register" element={<Register />} />
-            <Route path="verify" element={<Verification />}/>
-          </Route>
-
-        </Routes>
-      </BrowserRouter>
+   
+     <BrowserRouter>
+  <Routes>
+    {isAuthenticated ? (
+      <Route path="/auth" element={<AuthPage />}>
+        <Route path="tam/resetPassword/:token" element={<ResetPassword />} />
+        <Route path="tam/registration/:token" element={<SavePasword />} />
+        <Route path="apartmentpage" element={<ApartmentPage />} />
+        <Route path="forgetpassword" element={<ForgetPassword />} />
+        <Route path="verify" element={<Verification/>} />
+        <Route path="apartmentcard/:id" element={<ApartmentCard />} />
+        <Route path="*" element={<Navigate to="apartmentpage" replace />} />
+      </Route>
+    ) : (
+      <Route path="/auth" element={<AuthPage />}>
+      <Route path="/auth/login" element={<Login />} index />
+      <Route path="/auth/register" element={<Register/>} />
+      <Route path="*" element={<Navigate to="/auth/login" replace />} /> 
+      </Route>
+    )}
+   
+  </Routes>
+</BrowserRouter>
     </>
   );
 };
