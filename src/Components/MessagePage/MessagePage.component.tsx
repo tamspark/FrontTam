d05be59ff72ba2af1873a55faa69fb58297f751b
giselathellipi <109949845+giselathellipi@users.noreply.github.com>
2023-11-15 12:30:23 +0100

    create the incoming message page

--- src/Components/MessagePage/MessagePage.component.tsx
+++ src/Components/MessagePage/MessagePage.component.tsx
@@ -1,15 +1,70 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 import ExistingChat from "Components/ExistingChat/ExistingChat.component";
 import MessageBox from "Components/MessageBox/MessageBox.component";
 
-import { MessagePageContainer } from "./style/MessagePage.style";
-
+import {
+  MessageLi,
+  MessagePageContainer,
+  MiniNotifyHolder,
+  NotificationHolder,
+  Subject,
+} from "./style/MessagePage.style";
+import {
+  fetchMessage,
+  MesagePageProps,
+} from "redux/MessagePage/MessagePageSlice";
+import { AppDispatch, RootState } from "redux/store";
+import { useDispatch, useSelector } from "react-redux";
+interface Notification {
+  subject: string;
+  message: string;
+}
 const MessagePage: FC<{}> = () => {
+  const [notifications, setNotifications] = useState<MesagePageProps[]>([]);
+  console.log(notifications);
+  const dispatch: AppDispatch = useDispatch();
+  const userId = useSelector((state: RootState) => state.auth.user?.id);
+  console.log(userId);
+  //get api
+  useEffect(() => {
+    if (userId) {
+      dispatch(fetchMessage({ userId }))
+        .then((result: any) => {
+          if (fetchMessage.fulfilled.match(result)) {
+            setNotifications([result.payload]);
+          } else {
+            console.error("Messages not found.");
+          }
+        })
+        .catch((error: any) => {
+          console.error("Error fetching messages:", error);
+        });
+    }
+  }, [dispatch, userId]);
   return (
     <MessagePageContainer>
       <ExistingChat />
-      <MessageBox />
+      <NotificationHolder>
+        {notifications.map((notification, index) => (
+          <MiniNotifyHolder key={index}>
+            {notification.messages.map((message, messageIndex) => (
+              <MessageLi
+                key={messageIndex}
+                style={{
+                  background:
+                    messageIndex % 2 === 0 ? "#5f59593d" : " #c1bbbb00",
+                }}
+              >
+                <Subject>{message.subject}</Subject>
+                {message.message}
+              </MessageLi>
+            ))}
+          </MiniNotifyHolder>
+        ))}
+      </NotificationHolder>
+
+      {/* <MessageBox /> */}
     </MessagePageContainer>
   );
 };
--- src/Components/MessagePage/style/MessagePage.style.tsx
+++ src/Components/MessagePage/style/MessagePage.style.tsx
@@ -5,3 +5,20 @@ export const MessagePageContainer = styled.div`
   width: 100%;
   height: 100%;
 `;
+
+export const NotificationHolder = styled.div`
+  background-color: #ffff;
+  width: 100%;
+  overflow: scroll;
+`;
+export const MiniNotifyHolder = styled.div`
+  background-color: #cfcccc;
+`;
+export const Subject = styled.p`
+  margin: 0;
+  padding: 10px;
+`;
+export const MessageLi = styled.p`
+  margin: 0;
+  padding: 10px;
+`;
